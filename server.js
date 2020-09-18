3b5f1f81ce51bb0f8f687f25d1841af63b17c82d
Raccoon2508 <ridge2006@mail.ru>
2020-09-18 12:54:53 +0300

    Build for deploy

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static(`./dist/youtube-client`));
 app.get(`/*`, function (req, res) {
-    res.sendFile(`index.html`, { root: `./dist/youtube-client` })
+    res.sendFile(`index.html`, { root: `./dist/youtube-client` });
 });
 app.listen(process.env.PORT || 8080);
